dda2a060a5709de5e34f2129daa84ddaf0c8b4b6
haridanapana <haribabudanapana@gmail.com>
2025-03-24 16:32:02 +0530

    Update dragAndDrop.spec.ts

--- mytests/dragAndDrop.spec.ts
+++ mytests/dragAndDrop.spec.ts
@@ -2,7 +2,7 @@ const {webkit,firefox,chromium}= require('playwright');
 import { test, expect,Browser,Page } from '@playwright/test';
 
 test('dragAndDrop test',async()=>{
-    const browser:Browser=await chromium.launch({headless: false});
+    const browser:Browser=await chromium.launch({headless: true});
     const page: Page = await browser.newPage();
     await page.goto('https://jqueryui.com/resources/demos/droppable/default.html');
     //drag and drop
